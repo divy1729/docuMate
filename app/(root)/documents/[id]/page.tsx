62b762d2dc449cba9e1aa59b0fe533acda8879ac
divy1729 <155442934+divy1729@users.noreply.github.com>
2024-08-30 22:57:56 +0530

    document editor initialisation

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+import { Editor } from '@/src/components/editor/Editor'
+import React from 'react'
+
+const Document = () => {
+  return (
+    <div>
+      <Header>
+        <div className='text-white-28'>Document</div></Header><Editor /></div>
+  )
+}
+
+export default Document
