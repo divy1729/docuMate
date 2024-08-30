57ba727dd6829a777fde1b064bc68893b5e551d5
divy1729 <155442934+divy1729@users.noreply.github.com>
2024-08-30 18:00:31 +0530

    setup done
     installed next,shadcn,

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import "./globals.css"
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+import { title } from "process"
+import { Metadata } from "next"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+export const metadata: Metadata = {
+    title:'Prapatra',
+    description:'Your go-to collaborative editor',
+    
+}
+
+export default function RootLayout({ children }: {children : React.ReactNode}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+        ...
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default Home
